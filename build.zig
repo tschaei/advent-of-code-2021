981fcc3f5e8d58b11faf8640522aabb200556f05
tschaei_ <jakob.falke@mailbox.org>
2021-12-09 09:00:42 +0100

    clean up build file a bit. Don't fear the allocator.

--- build.zig
+++ build.zig
@@ -1,25 +1,26 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
-    makeBuildForDay(b, target, mode, "01");
-    makeBuildForDay(b, target, mode, "02");
-    makeBuildForDay(b, target, mode, "03");
-    makeBuildForDay(b, target, mode, "04");
-    makeBuildForDay(b, target, mode, "05");
-    makeBuildForDay(b, target, mode, "06");
-    makeBuildForDay(b, target, mode, "07");
-    makeBuildForDay(b, target, mode, "08");
-    makeBuildForDay(b, target, mode, "09");
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    try makeBuildForDay(allocator, b, target, mode, "01");
+    try makeBuildForDay(allocator, b, target, mode, "02");
+    try makeBuildForDay(allocator, b, target, mode, "03");
+    try makeBuildForDay(allocator, b, target, mode, "04");
+    try makeBuildForDay(allocator, b, target, mode, "05");
+    try makeBuildForDay(allocator, b, target, mode, "06");
+    try makeBuildForDay(allocator, b, target, mode, "07");
+    try makeBuildForDay(allocator, b, target, mode, "08");
+    try makeBuildForDay(allocator, b, target, mode, "09");
 }
 
-pub fn makeBuildForDay(b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode, day: []const u8) void {
-    var step_name = [_]u8{undefined} ** 5;
-    _ = std.fmt.bufPrint(&step_name, "day{s}", .{day}) catch unreachable;
-    var step_src = [_]u8{undefined} ** 13;
-    _ = std.fmt.bufPrint(&step_src, "src/{s}.zig", .{step_name}) catch unreachable;
-    const step = b.addExecutable(&step_name, &step_src);
+pub fn makeBuildForDay(allocator: std.mem.Allocator, b: *std.build.Builder, target: std.zig.CrossTarget, mode: std.builtin.Mode, day: []const u8) !void {
+    const stepName = try std.fmt.allocPrint(allocator, "day{s}", .{day});
+    const stepSrc = try std.fmt.allocPrint(allocator, "src/{s}.zig", .{stepName});
+    const step = b.addExecutable(stepName, stepSrc);
     step.setTarget(target);
     step.setBuildMode(mode);
     step.install();
@@ -28,10 +29,8 @@ pub fn makeBuildForDay(b: *std.build.Builder, target: std.zig.CrossTarget, mode:
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-    var run_step_name = [_]u8{undefined} ** 9;
-    _ = std.fmt.bufPrint(&run_step_name, "run_day{s}", .{day}) catch unreachable;
-    var run_desc = [_]u8{undefined} ** 9;
-    _ = std.fmt.bufPrint(&run_desc, "Run {s}", .{step_name}) catch unreachable;
-    const run_step = b.step(&run_step_name, &run_desc);
+    var runStepName = try std.fmt.allocPrint(allocator, "run_day{s}", .{day});
+    var runDesc = try std.fmt.allocPrint(allocator, "Run {s}", .{stepName});
+    const run_step = b.step(runStepName, runDesc);
     run_step.dependOn(&run_cmd.step);
 }
